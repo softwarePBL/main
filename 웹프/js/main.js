854ec4d72c40a1b53a74eeec2b3688f78e82d63c
Yulllll1 <127711162+Yulllll1@users.noreply.github.com>
2023-04-10 11:41:22 +0900

    Add files via upload

---
+++ 웹프/js/main.js
@@ -0,0 +1,8 @@
+function dp_menu(){
+    let click = document.getElementById("more_coffee");
+    if(click.style.display === "none"){
+        click.style.display = "block";
+    }else{
+        click.style.display = "none";
+    }
+}
