44044e6ee1b61348d2f3c28da9a99a5f99649ad7
Yulllll1 <127711162+Yulllll1@users.noreply.github.com>
2023-05-26 11:04:11 +0900

    Add files via upload

--- 웹프/js/product.js
+++ 웹프/js/product.js
@@ -1,13 +1,41 @@
-var products = ['상품 1', '상품 2', '상품 3', '상품 4', '상품 5', '상품 6',
-                '상품 7', '상품 8', '상품 9', '상품 10', '상품 11', '상품 12',
-                '상품 13', '상품 14', '상품 15', '상품 16', '상품 17', '상품 18',
-                '상품 19', '상품 20'];
+var products = [
+    {title: '상품 1', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 2', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 3', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 4', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 5', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 6', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+    {title: '상품 7', image: '상품1.jpg', description: '상품 1에 대한 설명'},
+];
 
 var productList = document.getElementById('productList');
 
-for (var i = 0; i < products.length; i++){
+for (var i = 0; i < products.length; i++) {
     var productItem = document.createElement('div');
     productItem.classList.add('product-item');
-    productItem.textContent = products[i];
+    
+    var titleElement = document.createElement('h3');
+    titleElement.textContent = products[i].title;
+    productItem.appendChild(titleElement);
+
+    var descriptionElement = document.createElement('p');
+    descriptionElement.textContent = products[i].description;
+    productItem.appendChild(descriptionElement);
+
+
+    var imageElement = document.createElement('img');
+    imageElement.src = products[i].image;
+    productItem.appendChild(imageElement);
+    
     productList.appendChild(productItem);
 }
+
+
+
