18eef667113b83c4d38586370cadaf4e4ce90ff2
Yulllll1 <127711162+Yulllll1@users.noreply.github.com>
2023-06-01 00:18:19 +0900

    Delete 웹프 directory

--- 웹프/js/myPageCart.js
+++
@@ -1,8 +0,0 @@
-function selectAll(selectAll)  {
-    const checkboxes 
-       = document.querySelectorAll('input[type="checkbox"]');
-    
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = selectAll.checked
-    })
-  }
